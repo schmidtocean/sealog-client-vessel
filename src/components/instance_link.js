8c7d22959ee8b81842b890a8f203c6beb6a06fd3
Marine Technician <mt@sealog-24.c.falkor-science-system.internal>
2026-07-21 11:37:22 +0000

    fixing defaults and instance links

--- src/components/instance_link.js
+++ src/components/instance_link.js
@@ -32,9 +32,9 @@ class InstanceLink extends Component {
 
     return (
       <OverlayTrigger placement={placementTT} instance={this.props.instance} delay={{ show: 250, hide: 400 }} overlay={this.renderTooltip}>
-        <Link className={placementCN} to={{ pathname: `./${this.props.instance}` }}>
+        <a className={placementCN} href={`/${this.props.instance}`}>
           <Image style={{ height: '30px' }} src={imageFN} />
-        </Link>
+        </a>
       </OverlayTrigger>
     )
   }
