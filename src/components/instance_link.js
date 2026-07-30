d8fbb0ea49c0c934316b4b9b42f8fa01cfccccab
Webb Pinner <webbpinner@gmail.com>
2026-07-30 17:10:08 +0000

    updated instance links

--- src/components/instance_link.js
+++ src/components/instance_link.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
 import { Image, OverlayTrigger, Tooltip } from 'react-bootstrap'
 import { ROOT_PATH } from '../client_settings'
